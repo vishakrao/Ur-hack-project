810c44fb4c16b03a2b1eab0b0325b7937914b363
SUHAS N H <suhas4497@gmail.com>
2022-11-05 11:58:12 +0000

    Update 15 files(Slider)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import "./components/style.css";
 import "./components/Header.css";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Navbar1 from "./components/Navbar";
-import CardWrapper from "./components/CardWrapper"
+import CardWrapper from "./components/CardWrapper";
+import BasicSlider from "./Slider.js";
+import "./index.css";
 
 export default function App() {
   return (
     <div>
       <div className="Header">
-      <Header/>
+        <Header />
+        <BasicSlider />
       </div>
       <div className="main-cover">
-      <div>
-      <Navbar1/>
+        <div>
+          <Navbar1 />
+        </div>
+        <div className="grid-wrap">
+          <CardWrapper />
+        </div>
       </div>
-      <div className="grid-wrap">
-      <CardWrapper/>
-
-      </div>
-    </div>
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,19 @@
-import React, {useState} from 'react'
-import { Card, Image, Text, Button} from "@mantine/core"
+import React from "react";
+import { Card, Image, Text, Button } from "@mantine/core";
 
 export default function Cards(props) {
-  
   // const [randNum ,setRandNum] = useState(0)
   // setRandNum(Math.floor(Math.random() * 10) + 1 )
-    
+
   return (
     <>
-      <Card className='box' withBorder>
+      <Card className="box" withBorder>
         <Image className="wh" src={props.url} alt="" />
         <Text> {props.amt} Eth</Text>
-        
+
         {/* Add button functionalities - ethers.js */}
-        <Button> Buy Now </Button> 
+        <Button> Buy Now </Button>
       </Card>
     </>
-  )
+  );
 }
